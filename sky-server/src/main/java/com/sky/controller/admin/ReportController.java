f9e2390cd2a7d298092d62d9023cf29460a18121
One <jiangya68@gmail.com>
2023-12-15 16:12:34 +0800

    营业额数据统计： 根据map集合查询mybatis

---
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -0,0 +1,47 @@
+package com.sky.controller.admin;
+
+
+import com.sky.result.Result;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ *  图表报告
+ */
+@RestController
+@Slf4j
+@RequestMapping("/admin/report")
+@Api(tags = "")
+public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+
+
+    /**
+     * 营业额数据统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/turnoverStatistics")
+    public Result<TurnoverReportVO> turnoverStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end
+            ){
+        return Result.success(reportService.getTurnover(begin,end));
+    }
+
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,67 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    /**
+     * 营业额数据统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnover(LocalDate begin, LocalDate end) {
+
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        List<Double> list = new ArrayList<>();
+        dateList.forEach(date -> {
+            LocalDateTime beginTime = LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date,LocalTime.MAX);
+            Map map = new HashMap<>();
+            map.put("status", Orders.COMPLETED);
+            map.put("begin",beginTime);
+            map.put("end",endTime);
+            Double turnover = orderMapper.sumByMap(map);
+            turnover = (turnover == null) ? 0.0 : turnover;
+            list.add(turnover);
+        });
+
+        return TurnoverReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .turnoverList(StringUtils.join(list,","))
+                .build();
+        /**
+         *     //日期，以逗号分隔，例如：2022-10-01,2022-10-02,2022-10-03
+         *     private String dateList;
+         *
+         *     //营业额，以逗号分隔，例如：406.0,1520.0,75.0
+         *     private String turnoverList;
+         */
+    }
+}
